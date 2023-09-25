f6cd7fe3512aaeaadf2763b5559a000274a2239f
Oscar Carvente <oscar.carvente@gmail.com>
2023-09-24 21:47:38 -0700

    Migrate controls to typescript

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,6 +1,9 @@
-import PropTypes from 'prop-types';
 
-const ControlPanel = ({ name, onChange }) => {
+type ControlPanelProps = {
+  name: string,
+  onChange: React.ChangeEventHandler<HTMLInputElement>
+}
+const ControlPanel = ({ name, onChange }: ControlPanelProps) => {
   return (
     <form
       className="flex flex-row gap-4 text-white bg-primary-10"
@@ -20,9 +23,5 @@ const ControlPanel = ({ name, onChange }) => {
   );
 };
 
-ControlPanel.propTypes = {
-  name: PropTypes.string,
-  onChange: PropTypes.func,
-};
 
 export default ControlPanel;
